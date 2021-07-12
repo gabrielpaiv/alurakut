0f8879a431de14e964066d0b984e30d209d26c47
gabrielpaiv <gabriel.alcantara.ti@gmail.com>
2021-07-12 13:39:50 -0300

    First Commit

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
-import styled from 'styled-components'
+import MainGrid from "../src/components/MainGrid"
+import Box from "../src/components/Box"
+import { AlurakutMenu } from "../src/lib/AlurakutCommons"
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+function ProfileSidebar(propriedades) {
+  return (
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />
+    </Box>
+  )
+}
 
 export default function Home() {
-  return <Title>My page</Title>
+  const usuario = 'gabrielpaiv';
+  const pessoasFavoritas = ['juunegreiros',
+    'peas',
+    'omariosouto',
+    'rafaballerini',
+    'marcobrunodev',
+    'felipefialho'
+  ]
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar githubUser={usuario} />
+        </div>
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>Bem vindo</Box>
+        </div>
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <Box>
+            <h2 className="smallTitle">
+              Pessoas da Comunidade ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <a href={`/users/${itemAtual}`} key={itemAtual}>
+                    <img src={`https://github.com/${itemAtual}.png`} />
+                    <span>{itemAtual}</span>
+                  </a>
+                )
+              })}
+            </ul>
+          </Box>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </>
+  )
 }
